86857fcdc21e9372d50ce2993e524d8f76bf3afb
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-09-09 00:12:51 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,51 +1,38 @@
-#!/usr/bin/python3
-"""fab script that deploy compres file """
-from fabric.api import *
-from datetime import datetime
-import os
+from fabric.api import env, put, run
+from os.path import exists
 
-
-env.hosts = ["54.236.27.183", "34.229.56.92"]
-env.user = "ubuntu"
-
-
-def do_pack():
-    """ func compres static web folder"""
-    n = datetime.now()
-    t = f"web_static_{n.year}{n.month}{n.day}{n.hour}{n.minute}{n.second}.tgz"
-    local("mkdir -p versions")
-    with lcd("./versions"):
-        res = local(f"tar -czvf {t} ../web_static")
-        if res.succeeded:
-            return f"versions/{t}"
-    return None
+# Configure the web server IPs
+env.hosts = ['34.229.56.92', '54.236.27.183>']
+# Set the SSH username
+env.user = 'ubuntu'
+# Set the path to the web server directory
+env.deploy_path = '/data/web_static'
 
 
 def do_deploy(archive_path):
-    """func deploy archive file """
-    if not os.path.exists(archive_path):
+    if not exists(archive_path):
         return False
-    res = put(local_path=archive_path, remote_path="/tmp/")
-    if res.failed:
+
+    try:
+        # Upload the archive to the web server
+        put(archive_path, '/tmp/')
+        
+        # Extract the archive to a new release folder
+        archive_filename = archive_path.split('/')[-1]
+        release_folder = '/'.join(archive_filename.split('.')[:-1])
+        release_path = '{}/releases/{}'.format(env.deploy_path, release_folder)
+        run('mkdir -p {}'.format(release_path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
+        
+        # Remove the uploaded archive
+        run('rm /tmp/{}'.format(archive_filename))
+        
+        # Remove the current symbolic link
+        run('rm -f {}/current'.format(env.deploy_path))
+        
+        # Create a new symbolic link to the new release
+        run('ln -s {} {}/current'.format(release_path, env.deploy_path))
+        
+        return True
+    except:
         return False
-    file_name = archive_path.split("/")[-1]
-    remote_path = f"/tmp/{file_name}"
-    with cd("/data/web_static/releases/"):
-        res = sudo(f"tar -xzvf {remote_path}")
-        if res.failed:
-            return False
-    res = sudo(f"rm {remote_path}")
-    if res.failed:
-        return False
-    res = sudo("rm /data/web_static/current")
-    if res.failed:
-        return False
-    folder_old = "/data/web_static/releases/web_static"
-    folder_new = f"/data/web_static/releases/{file_name.split('.')[0]}"
-    res = sudo(f"mv {folder_old} {folder_new}")
-    if res.failed:
-        return False
-    res = sudo(f"ln -s {folder_new} /data/web_static/current")
-    if res.failed:
-        return False
-    return True
