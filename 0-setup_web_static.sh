41621b399ac0db1c6efbf8b248682ddec95460a6
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-09-09 00:05:47 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,20 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+#script sets up webstatic
 
-apt-get update
-apt-get install -y nginx
-
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://github.com/besthor;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "this is simple content" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+content=$(cat << 'END HEREDOC'
+	location /hbnb_static {
+		alias /data/web_static/current/;
+	}
+END HEREDOC
+)
+echo -e "$content" >> text.txt
+sudo sed -i '/server_name _;/r text.txt' /etc/nginx/sites-available/default
+sudo rm text.txt
+sudo service nginx restart
